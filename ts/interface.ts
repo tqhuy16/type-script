3742b2ce577081f4ca989544c1d745ce0f96d4ca
Quang Huy <tqhuy16@gmail.com>
2022-03-08 17:34:45 +0700

    no commit

--- ts/interface.ts
+++ ts/interface.ts
@@ -8,3 +8,41 @@ function speak(food: Food): void {
 }
 
 speak({calories: 250, name: "rice"})
+
+/**----------------------------------- */
+interface People {
+  name: string;
+  eat(): void;
+  sleep(): void;
+}
+interface Bird {
+  fly(): void
+}
+
+class Machine {
+  calculate(x: number, y: number): number {
+    return x + y;
+  }
+}
+class Superman extends Machine implements People, Bird {
+  name: string;
+  constructor(name: string){
+    super()
+    this.name = name;
+  }
+  eat(): void {
+    console.log(`${this.name} eat`)
+  }
+  sleep(): void {
+    console.log(`${this.name} sleep`)
+  }
+  fly(): void {
+    console.log(`${this.name} fly`)
+  }
+}
+
+let huy: Superman = new Superman('huy');
+huy.eat()
+huy.sleep()
+huy.fly()
+console.log(huy.calculate(2, 3))
